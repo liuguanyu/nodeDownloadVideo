7bd2c2990a53940d42b4d3fa203d67a94c666065
liuguanyu <liuguanyu@localhost.localdomain>
2015-05-07 18:01:19 +0800

    -

--- index.js
+++ index.js
@@ -4,118 +4,125 @@ var fs = require("fs");
 
 var path = require("path");
 
-var dirName = +(new Date()) + "";
-
-var workPath = path.resolve(__dirname, '.', dirName);
-
-if (!fs.existsSync(workPath)){
-	fs.mkdirSync(workPath);
-}
-
 var exec = require('child_process').exec;
 
-parser('http://www.iqiyi.com/v_19rrnseanw.html', '', function(err, data){
-    var title = data["title"];
-    var datas = data["高清"];
-    var length = datas.length;
-    var total = datas.length;
+var downloadInfo = fs.readFileSync("download.txt").toString();
 
-    var qiyiDownload = function (jsonFile, callback){
-	    var JsonObj = JSON.parse(fs.readFileSync(jsonFile));
-	    var m = jsonFile.match(/\/(\d+?)\.json$/);
-	    var i = m[1];
+var downloadList = downloadInfo.split("\n");
 
-	    var videoFile = path.resolve(workPath, '.', i + ".f4v");
+downloadList.forEach(function (el){
+    el = el.trim();
+    parser(el, '', function(err, data){
+        var dirName = +(new Date()) + "";
 
-	    var cmd = "wget -O " + videoFile +  " '" +JsonObj.l + "'";
+        var workPath = path.resolve(__dirname, '.', dirName);
 
-        exec(cmd, function (err, stdout, stderr){
-        	   if (err){
-        	   	  callback({
-        	   	  	index : i,
-        	   	  	status : "error"
-        	   	  });
-        	   }
-        	   else{
-        	   	  callback({
-        	   	  	index : i,
-        	   	  	status : "ok"
-        	   	  });
-        	   }
-        });
-    };
+        if (!fs.existsSync(workPath)){
+          fs.mkdirSync(workPath);
+        }
 
-    // 这里采用http://yonsm.net/mp4merge/的第三种方法解决
-    var merge = function (length){
-        var tss = [];
-    	for(var i = 0 ; i < length; i++){
-           tss.push(path.resolve(workPath, '.', i + ".ts"));
-    	}
+        var title = data["title"];
+        var datas = data["高清"];
+        var length = datas.length;
+        var total = datas.length;
 
-        var targetFile = path.resolve(workPath, '.', "output.mp4");
-        var finalFile = path.resolve(__dirname, '.', title + ".mov");
+        var qiyiDownload = function (jsonFile, callback){
+    	    var JsonObj = JSON.parse(fs.readFileSync(jsonFile));
+    	    var m = jsonFile.match(/\/(\d+?)\.json$/);
+    	    var i = m[1];
 
-        var cmd = [];
-        cmd.push('ffmpeg -i "concat:' + tss.join("|") + '" -acodec copy -vcodec copy -absf aac_adtstoasc ' + targetFile);
-        cmd.push('ffmpeg -i ' + targetFile + " " + finalFile);
-        cmd.push('rm -fr ' + workPath);
+    	    var videoFile = path.resolve(workPath, '.', i + ".f4v");
 
-        var finalCmd = cmd.join(";");
-        exec(finalCmd, function (err, stdout, stderr){
-           if (err){
-           }
-           else{
-           }
-        });
-    };
+    	    var cmd = "wget -O " + videoFile +  " '" +JsonObj.l + "'";
 
-    var qiyiEncode = function (i, callback){
-        var sourceFile = path.resolve(workPath, '.', i + ".f4v");
-        var targetFile = path.resolve(workPath, '.', i + ".mp4");
-        var tsFile = path.resolve(workPath, '.', i + ".ts");
-        var cmd = [];
-        //cmd[0] = "ffmpeg -i " + sourceFile + " " + targetFile;
-        cmd.push("ffmpeg -i " + sourceFile + " -vcodec copy -acodec copy -vbsf h264_mp4toannexb " + tsFile);
-        //cmd[1] = "rm " + targetFile;
-        cmd.push("rm " + sourceFile);
+            exec(cmd, function (err, stdout, stderr){
+            	   if (err){
+            	   	  callback({
+            	   	  	index : i,
+            	   	  	status : "error"
+            	   	  });
+            	   }
+            	   else{
+            	   	  callback({
+            	   	  	index : i,
+            	   	  	status : "ok"
+            	   	  });
+            	   }
+            });
+        };
 
-        var finalCmd = cmd.join(";");
+        // 这里采用http://yonsm.net/mp4merge/的第三种方法解决
+        var merge = function (length){
+            var tss = [];
+        	for(var i = 0 ; i < length; i++){
+               tss.push(path.resolve(workPath, '.', i + ".ts"));
+        	}
 
-        exec(finalCmd, function (err, stdout, stderr){
+            var targetFile = path.resolve(workPath, '.', "output.mp4");
+            var finalFile = path.resolve(__dirname, '.', title + ".mov");
+
+            var cmd = [];
+            cmd.push('ffmpeg -i "concat:' + tss.join("|") + '" -acodec copy -vcodec copy -absf aac_adtstoasc ' + targetFile);
+            cmd.push('ffmpeg -i ' + targetFile + " " + finalFile);
+            cmd.push('rm -fr ' + workPath);
+
+            var finalCmd = cmd.join(";");
+            exec(finalCmd, function (err, stdout, stderr){
                if (err){
-                  callback({
-                    index : i,
-                    status : "error"
-                  });
                }
                else{
-                  callback({
-                    index : i,
-                    status : "ok"
-                  });
                }
-        });
-    };
+            });
+        };
 
-    datas.forEach(function (el, i){
-    	var jsonFile = path.resolve(workPath, '.', i + ".json");
-        var cmd = "wget -O " + jsonFile + " '"  + el + "'";
+        var qiyiEncode = function (i, callback){
+            var sourceFile = path.resolve(workPath, '.', i + ".f4v");
+            var targetFile = path.resolve(workPath, '.', i + ".mp4");
+            var tsFile = path.resolve(workPath, '.', i + ".ts");
+            var cmd = [];
+            //cmd[0] = "ffmpeg -i " + sourceFile + " " + targetFile;
+            cmd.push("ffmpeg -i " + sourceFile + " -vcodec copy -acodec copy -vbsf h264_mp4toannexb " + tsFile);
+            //cmd[1] = "rm " + targetFile;
+            cmd.push("rm " + sourceFile);
 
-        exec(cmd, function (err, stdout, stderr){
-        	   if (err){
-        	   	  console.log("Running " + cmd + " cause error!");
-        	   }
-        	   else{
-        	   	  qiyiDownload(jsonFile, function (data){
-                      qiyiEncode(i, function(){
-                          --length;
+            var finalCmd = cmd.join(";");
 
-                          if (length == 0){
-                            merge(total);
-                          }
+            exec(finalCmd, function (err, stdout, stderr){
+                   if (err){
+                      callback({
+                        index : i,
+                        status : "error"
                       });
-        	   	  });
-        	   }
+                   }
+                   else{
+                      callback({
+                        index : i,
+                        status : "ok"
+                      });
+                   }
+            });
+        };
+
+        datas.forEach(function (el, i){
+        	var jsonFile = path.resolve(workPath, '.', i + ".json");
+            var cmd = "wget -O " + jsonFile + " '"  + el + "'";
+
+            exec(cmd, function (err, stdout, stderr){
+            	   if (err){
+            	   	  console.log("Running " + cmd + " cause error!");
+            	   }
+            	   else{
+            	   	  qiyiDownload(jsonFile, function (data){
+                          qiyiEncode(i, function(){
+                              --length;
+
+                              if (length == 0){
+                                merge(total);
+                              }
+                          });
+            	   	  });
+            	   }
+            });
         });
     });
 });
